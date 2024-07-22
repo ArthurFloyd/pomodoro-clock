c7120b411b4b9f14d1263f83583b6ac8b74727f8
arthur voryshev <what_to_write@mail.ru>
2024-07-22 22:21:50 +1000

    install circular-progress-bar

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,7 +1,21 @@
+import CircularProgressBar from "circular-progress-bar";
+
+// const options = {
+//   size: 200,
+//   background: "transparent"
+// };
+// const progress = new CircularProgressBar(42, options);
+// progress.appendTo(document.body);
+const gauge = new CircularProgressBar();
+gauge.value = 20; // Change value to 20%
+// or
+gauge.values = [10, 20, 30]; // Change values to 10%, 20% and 30% (multiple bars)
+
+console.log(gauge.values);
 function Timer() {
   return (
     <div>
-      test
+      {gauge.values}
     </div>
   )
 }
