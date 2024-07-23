4a9ebba2a41c73fee200a4b3f96767d762bc6893
arthur voryshev <what_to_write@mail.ru>
2024-07-23 16:40:08 +1000

    uninstall circular-progress-bar and install react-circular-progressbar

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,21 +1,35 @@
-import CircularProgressBar from "circular-progress-bar";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
-// const options = {
-//   size: 200,
-//   background: "transparent"
-// };
-// const progress = new CircularProgressBar(42, options);
-// progress.appendTo(document.body);
-const gauge = new CircularProgressBar();
-gauge.value = 20; // Change value to 20%
-// or
-gauge.values = [10, 20, 30]; // Change values to 10%, 20% and 30% (multiple bars)
+const percentage = 66;
 
-console.log(gauge.values);
 function Timer() {
   return (
-    <div>
-      {gauge.values}
+    <div style={{ width: 200, height: 200 }}>
+      <CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        styles={buildStyles({
+          // Rotation of path and trail, in number of turns (0-1)
+          rotation: 0.25,
+
+          // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+          strokeLinecap: 'butt',
+
+          // Text size
+          textSize: '16px',
+
+          // How long animation takes to go from one percentage to another, in seconds
+          pathTransitionDuration: 0.5,
+
+          // Can specify path transition in more detail, or remove it entirely
+          // pathTransition: 'none',
+
+          // Colors
+          pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+          textColor: '#f88',
+          trailColor: '#d6d6d6',
+          backgroundColor: '#3e98c7',
+        })} />
     </div>
   )
 }
